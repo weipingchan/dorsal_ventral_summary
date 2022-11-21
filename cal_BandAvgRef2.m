e8d6870298bc1531ed54a25de9b70d193a1e6505
WPC <w.p.chan23@gmail.com>
2022-11-21 00:20:15 -0500

    Missing value handling

--- cal_BandAvgRef2.m
+++ cal_BandAvgRef2.m
@@ -1,32 +1,35 @@
-function sidesAllBandRef=cal_BandAvgRef(fore_hind_grids_dorsal)
+function sidesAllBandRef=cal_BandAvgRef2(fore_hind_grids_dorsal)
 %Derive the most variant region
 wingPartLoc=[1,4];
 sidesAllBandRef=cell(0,2);    
 for wingID=1:2
     allBandStability=cell(0,10);    
     for bandID=1:10
-        detDat=fore_hind_grids_dorsal{1}{wingPartLoc(wingID)}{bandID};
+        for matID=1:length(fore_hind_grids_dorsal)
+            detDat=fore_hind_grids_dorsal{matID}{wingPartLoc(wingID)}{bandID};
+            if length(detDat)>4
+                break
+            end
+        end
         if size(detDat,3)==1
+            bandDatAll=[];
             for matID=1:length(fore_hind_grids_dorsal)
                 meanRefDat=fore_hind_grids_dorsal{matID}{wingPartLoc(wingID)}{bandID};
-                    if matID==1
-                        bandDatAll=meanRefDat;
-                    else
+                if length(meanRefDat)>4
                         bandDatAll=cat(3,bandDatAll,meanRefDat);
-                    end
+                end
             end
             bandStability=mean(bandDatAll,3,'omitnan');
         else
+            bandDatAllR=[];
+            bandDatAllG=[];
+            bandDatAllB=[];
            for matID=1:length(fore_hind_grids_dorsal)
                 meanRefDat=fore_hind_grids_dorsal{matID}{wingPartLoc(wingID)}{bandID};
-                if matID==1
-                    bandDatAllR=meanRefDat(:,:,1);
-                    bandDatAllG=meanRefDat(:,:,2);
-                    bandDatAllB=meanRefDat(:,:,3);
-                else
-                    bandDatAllR=cat(3,bandDatAllR,meanRefDat(:,:,1));
-                    bandDatAllG=cat(3,bandDatAllR,meanRefDat(:,:,2));
-                    bandDatAllB=cat(3,bandDatAllR,meanRefDat(:,:,3));
+                if length(meanRefDat)>4
+                        bandDatAllR=cat(3,bandDatAllR,meanRefDat(:,:,1));
+                        bandDatAllG=cat(3,bandDatAllG,meanRefDat(:,:,2));
+                        bandDatAllB=cat(3,bandDatAllB,meanRefDat(:,:,3));
                 end
            end
            bandStability=cat(3,mean(bandDatAllR,3,'omitnan'),mean(bandDatAllG,3,'omitnan'),mean(bandDatAllB,3,'omitnan'));
